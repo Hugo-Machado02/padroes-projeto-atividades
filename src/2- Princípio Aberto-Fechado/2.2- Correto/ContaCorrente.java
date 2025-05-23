c27ab14c846813019f4ee4c49f3105ab089fcdbb
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-23 20:06:37 -0300

    ✨ feat: Adiciona método

--- src/2- Princípio Aberto-Fechado/2.2- Correto/ContaCorrente.java
+++ src/2- Princípio Aberto-Fechado/2.2- Correto/ContaCorrente.java
@@ -1,5 +1,8 @@
-public class ContaCorrente extends Conta{
+public class ContaCorrente extends Conta implements Rendimento{
     public ContaCorrente(String nome, String cpf) {
         super(nome, cpf);
+    }
+    public void CalculoRendimentos(){
+        System.out.println("Rendimento da conta poupança: 0.25%");
     }
 }
